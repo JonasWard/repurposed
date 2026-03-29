3cd4ae3825d5640c6dbda32d929231c70993eaa8
Jonas Ward <jonas.vandenbulcke@gmail.com>
2026-03-29 11:26:18 +0200

    feat: focus on select

--- components/leaflet/Map.tsx
+++ components/leaflet/Map.tsx
@@ -6,6 +6,9 @@ import 'leaflet-defaulticon-compatibility';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import { ListingData } from '@/lib/elements';
 import * as Leaflet from 'leaflet';
+import { useRouter } from 'next/router';
+import { useRepurposedStore } from '@/lib/store';
+import repurposedIcon from '/assets/icons/repurposed.svg';
 
 type LocatedListing = ListingData & { location: NonNullable<ListingData['location']> };
 
@@ -20,14 +23,18 @@ const getCenter = (elements: LocatedListing[]) =>
 
 export const Map: React.FC<{ elements: ListingData[]; className: string }> = ({ elements, className }) => {
   const located = elements.filter((e): e is LocatedListing => !!e.location);
+  const router = useRouter();
+  const setFocusedListingId = useRepurposedStore((s) => s.setFocusedListingId);
 
   if (located.length === 0) return null;
+
+  const isAllElements = router.pathname.includes('all-elements');
 
   return (
     <MapContainer
       className={className}
       center={getCenter(located)}
-      zoom={located.length > 1 ? 9 : 11}
+      zoom={4}
       scrollWheelZoom={true}
       attributionControl={false}
       zoomControl={false}
@@ -45,7 +52,14 @@ export const Map: React.FC<{ elements: ListingData[]; className: string }> = ({ 
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {located.map((element) => (
-        <Marker key={element._id} position={[element.location.lat, element.location.lng]} />
+        <Marker
+          icon={Leaflet.icon({ iconUrl: repurposedIcon.src, iconSize: [32, 32] })}
+          key={element._id}
+          position={[element.location.lat, element.location.lng]}
+          eventHandlers={isAllElements ? {
+            click: () => setFocusedListingId(element._id),
+          } : undefined}
+        />
       ))}
     </MapContainer>
   );
